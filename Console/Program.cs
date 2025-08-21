742e086d36b0d97a0956df682184b475175d3ff4
hrksh <r_hirakoshi@outlook.com>
2025-08-22 06:25:48 +0900

    Interface で振舞い実装

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Versioning;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 using System.Text;
 
 // See https://aka.ms/new-console-template for more information
@@ -23,25 +24,59 @@ namespace Common.Util
 
 namespace Prac.DesignPattern
 {
+    // 抽象クラスでやればいいって訳じゃないらしい
+    // こう？
+    
     /// <summary>
     /// カモ基底クラス
     /// </summary>
-    public class Duck
+    public abstract class Duck : IFlyBehavior, IQuackBehavior
     {
-        public void Quack() {}
+        private IQuackBehavior _quackBehavior;
+        private IFlyBehavior _flyBehavior;
+
+        public IQuackBehavior QuackBehavior
+        {
+            get => _quackBehavior;
+            set => _quackBehavior = value;
+        }
+
+        public IFlyBehavior FlyBehavior
+        {
+            get => _flyBehavior;
+            set => _flyBehavior = value;
+        }
+        
         public void Swim() {}
         public void Display() {}
-        public virtual bool Fly()
+
+        public bool Quack()
         {
-            return false;
+            return (bool)_quackBehavior?.Quack();
+        }
+        public bool Fly()
+        {
+            return (bool)_flyBehavior?.Fly();
         }
     }
 
     public class ReadHeadDuck : Duck
     {
-        public override bool Fly()
+        public ReadHeadDuck()
         {
-           return true;
+            // Java だと Interface が new できるっぽい？ C# じゃ無理そうなので別の方法を考える？
+            this.FlyBehavior = new IFlyBehavior();
+            this.QuackBehavior = new IQuackBehavior();
         }
+    }
+
+    public interface IFlyBehavior
+    {
+        public bool Fly();
+    }
+    
+    public interface IQuackBehavior
+    {
+        public bool Quack();
     }
 }
