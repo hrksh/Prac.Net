44e7939ada972b542c3abf11d535803063ef659b
r hrksh <r_hirakoshi@outlook.com>
2025-09-03 19:03:48 +0900

    非同期実装のステップ0

--- MyConsole/Prac/DesignPattern/Duck.cs
+++ MyConsole/Prac/DesignPattern/Duck.cs
@@ -2,43 +2,37 @@ using MyConsole.Prac.DesignPattern.Interface;
 
 namespace MyConsole.Prac.DesignPattern;
 
+// 一連でやってきたこれらは、StrategyPattern
+
+
 /// <summary>
 /// カモ基底クラス
 /// </summary>
 public abstract class Duck
 {
-    private IQuackBehavior _quackBehavior;
-    public  IQuackBehavior QuackBehavior
-    {
-        get => _quackBehavior;
-        set => _quackBehavior = value;
-    }
-    private IFlyBehavior _flyBehavior;
-    public IFlyBehavior FlyBehavior
-    {
-        get => _flyBehavior;
-        set => _flyBehavior = value;
-    }
+    public  IQuackBehavior QuackBehavior { get; set; }
+    public IFlyBehavior FlyBehavior { get; set; }
+
     public abstract void Display();
 
     /// <summary>
     /// Fly振舞いをセットする
     /// </summary>
     /// <param name="behavior">Flyインターフェイス</param>
-    public void SetFlyBehavior(IFlyBehavior behavior) => _flyBehavior = behavior;
+    public void SetFlyBehavior(IFlyBehavior behavior) => FlyBehavior = behavior;
     /// <summary>
     /// Quack振舞いをセットする
     /// </summary>
     /// <param name="behavior">Quackインターフェイス</param>
-    public void SetQuackBehavior(IQuackBehavior behavior) => _quackBehavior = behavior;
+    public void SetQuackBehavior(IQuackBehavior behavior) => QuackBehavior = behavior;
     
     // 継承先のクラスが利用するインターフェイスに委譲させる
     public bool Quack()
     {
-        return _quackBehavior.Quack();
+        return QuackBehavior.Quack();
     }
     public bool Fly()
     {
-        return _flyBehavior.Fly();
+        return FlyBehavior.Fly();
     }
 }
--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -5,15 +5,48 @@ using MyConsole.Prac.DesignPattern.Behavior;
 // See https://aka.ms/new-console-template for more information
 // System.Console.WriteLine("Hello World!");
 
-Duck testDuck = new ModelDuck();
 
 // おテスト
 
+var obj = new MyClass();
+
+obj.MethodA();
+obj.MethodB();
+obj.MethodC();
+
+System.Console.ReadLine();
+
+/*
+Duck testDuck = new ModelDuck();
 testDuck.Display();
 testDuck.Fly(); // まだ飛ばない
 testDuck.Quack();
 
 testDuck.SetFlyBehavior(new FlyRocketPowered());
 testDuck.Fly(); // 飛んだw
+*/
 
-System.Console.ReadLine();
+class MyClass()
+{
+    // 非同期処理の練習用
+
+    public void MethodA()
+    {
+        // 待ちたい処理
+        Console.WriteLine("MethodA Completed.");
+    }
+
+    public void MethodB()
+    {
+        // MethodA を待たずに終わらせたい処理
+        Console.WriteLine("MethodB Completed.");
+    }
+
+    public void MethodC()
+    {
+        // MethodA を待ってから完了させたい処理
+        Console.WriteLine("MethodC Completed.");
+    }
+}
+
+
