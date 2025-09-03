ff7775f5386128a712e55167baf983a12d6f0a93
r hrksh <r_hirakoshi@outlook.com>
2025-09-03 19:41:28 +0900

    Aのメソッドは誤りだったので修正

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -9,22 +9,10 @@ using MyConsole.Prac.DesignPattern.Behavior;
 // おテスト
 
 var obj = new MyClass();
-
 Task TaskA = obj.MethodA(); // TaskA が受け取れるよう非同期メソッド化
 await obj.MethodB();
 await obj.MethodC(TaskA); // TaskA を待ってから 完了させたいので、TaskA を渡して待たせる
-
 System.Console.ReadLine();
-
-/*
-Duck testDuck = new ModelDuck();
-testDuck.Display();
-testDuck.Fly(); // まだ飛ばない
-testDuck.Quack();
-
-testDuck.SetFlyBehavior(new FlyRocketPowered());
-testDuck.Fly(); // 飛んだw
-*/
 
 class MyClass()
 {
@@ -33,16 +21,19 @@ class MyClass()
     public async Task MethodA()
     {
         Console.WriteLine("MethodA Started.");
-        // 5秒間 待機する処理
-        await Task.Delay(5000);
+        
+        // 10秒間 待機する処理
+        for (int i = 1; i <= 10; i++)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine(i);
+        }
         Console.WriteLine("MethodA Completed.");
     }
 
     public async Task MethodB()
     {
         // MethodA を待たずに終わらせたい処理
-        // こいつは逆にすぐ終わらせたい。出力なら先にこいつが完了した表示がほしい。
-        // 先に終わらせたいので、この処理も非同期処理になる => async 化
         Console.WriteLine("MethodB Started.");
         await Task.Delay(5); // 5 msec の短い処理 を待ったら完了する
         Console.WriteLine("MethodB Completed.");
